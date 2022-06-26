bea079b615abe288a19cba5179c91e4b5821ce8c
Oyindamola27 <81050700+Oyindamola27@users.noreply.github.com>
2022-06-26 22:53:14 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var firstName,lastName,height,country;
+firstName ='Oyindamola';
+lastName ='Akinbote';
+height = '5 ft 10';
+country = 'Nigeria';
+alert(`${firstName} ${lastName} \n ${height} \n ${country}`)
